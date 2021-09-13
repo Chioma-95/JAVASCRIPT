21b0e9d287b525cf37583f6dc42748eca430c580
Chioma-95 <cf.onwukike@gmail.com>
2021-09-13 21:14:02 +0100

    Function

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function toFahrenheit (celsius) {
+    //var celsius = 50;    
+    console.log('Celsius: ' + celsius); 
+    var fahrenheit = (celsius * 9 / 5) + 32;  
+    console.log('Fahrenheit: ' + fahrenheit); 
+}
